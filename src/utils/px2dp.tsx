caecf220327cef9c2dfc1fe1b3312d6eae4092c5
gaowei1012 <gyb2020018@163.com>
2021-08-11 13:28:29 +0800

    fix px2dp

--- src/utils/px2dp.tsx
+++ src/utils/px2dp.tsx
@@ -2,9 +2,9 @@ import { Dimensions } from 'react-native'
 
 const defaultWidth: number = 375
 
-export const width = Dimensions.get('window').width
+export const width: number = Dimensions.get('window').width
 
-export const height = Dimensions.get('window').height
+export const height: number = Dimensions.get('window').height
 
 export const px2dp = (uiEleWidth: number) => {
   return (uiEleWidth * width) / defaultWidth
