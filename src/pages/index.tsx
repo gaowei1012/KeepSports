ce7e0ce3a7756aa66d92455702fd85a2f039c026
gaowei1012 <gyb2020018@163.com>
2021-08-11 12:50:51 +0800

    add pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+function Pages() {
+  return (
+    <View>
+      <Text>Pages</Text>
+    </View>
+  );
+}
+
+export default Pages;
