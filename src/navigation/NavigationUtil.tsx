a41746d3600fe8e636a169555191f5da57e8c941
gaowei1012 <gyb2020018@163.com>
2021-08-11 13:35:40 +0800

    add NavigationUtil

--- src/navigation/NavigationUtil.tsx
+++ src/navigation/NavigationUtil.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+export class NavigationUtil {
+  static navigation: any
+  /**
+   * 跳转到任意一级页面
+   * @param {any} params
+   * @param {elm} page
+   */
+  static goPage(params: any, page: any) {
+    let navigation = NavigationUtil.navigation
+    if (!navigation) {
+      return
+    }
+    if (!params) {
+      return
+    }
+    if (!page) {
+      return
+    }
+    navigation.navigate(page, { ...params })
+  }
+  /**
+   * 重置到首页
+   * @param {params} params params
+   */
+  static restToHomePage(params: any) {
+    const { navigation } = params
+    navigation.navigate('Main')
+  }
 
-function NavigationUtil() {
-  return (
-    <div>
-      NavigationUtil
-    </div>
-  );
+  /**
+   * 跳转到上一级页面
+   * @param navigation
+   * @returns {null}
+   */
+  /**
+   * 返回上一页
+   * @param navigation
+   */
+  static goBack(navigation: any) {
+    navigation.goBack()
+  }
+
+  // 返回
+  static goPop(navigation: any) {
+    navigation.pop()
+  }
 }
-
-export default NavigationUtil;
