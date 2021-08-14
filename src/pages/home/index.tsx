fbfac8ed044e66ad5abdc6093633bef4a7dc9809
gaowei1012 <gyb2020018@163.com>
2021-08-14 22:52:18 +0800

    添加首页顶部用户中心

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, ImageBackground } from 'react-native'
 import { styles } from '../../styles/view-style/home'
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.home_bg__container}>
+      <ImageBackground
+        style={styles.home_bg__container}
+        source={require('../../assets/pages/home/home_bg.png')}
+      >
+        <View style={styles.home_title__container}>
+          <Image style={styles.title__avatar} source={require('../../assets/pages/home/avatar.png')}/>
+          <View style={styles.title__flow}>
+            <Text style={styles.title__name}>遗失的美好</Text>
+            <Text style={styles.title__desc}>北京运动达人对</Text>
+          </View>
+        </View>
+      </ImageBackground>
+      {/* <View style={styles.home_bg__container}>
         <Image style={styles.bg_flow__container} source={require('../../assets/pages/home/home_bg.png')}/>
       </View>
+      <View>
+        <Text>jjj</Text>
+      </View> */}
+      {/* <View style={styles.swipple__flow}>
+
+      </View> */}
     </SafeAreaView>
   )
 }
--- src/styles/view-style/home.ts
+++ src/styles/view-style/home.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 执念
+ * @Date: 2021-08-13 21:35:50
+ * @LastEditTime: 2021-08-14 22:51:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /KeepSports/src/styles/view-style/home.ts
+ */
 import { StyleSheet } from 'react-native'
 import { px2dp, width, height } from '../../utils/px2dp'
 
@@ -11,5 +19,40 @@ export const styles = StyleSheet.create({
   bg_flow__container: {
     width: width,
     height: px2dp(248)
+  },
+  home_title__container: {
+    width: px2dp(345),
+    height: px2dp(52),
+    marginTop: px2dp(30),
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center'
+  },
+  title__avatar: {
+    width: px2dp(42),
+    height: px2dp(42)
+  },
+  title__flow: {
+    marginLeft: px2dp(10)
+  },
+  title__name: {
+    // fontFamily: 'SourceHanSansCN-Regular',
+    fontSize: px2dp(14),
+    marginBottom: px2dp(6),
+    fontWeight: '600',
+    color: '#fff'
+  },
+  title__desc: {
+    // fontFamily: 'SourceHanSansCN-Regular',
+    fontSize: px2dp(10),
+    fontWeight: '500',
+    color: '#fff'
+  },
+  swipple__flow: {
+    width: 345,
+    height: 153,
+    backgroundColor: 'red',
+    alignSelf: 'center',
+    marginTop: -150,
   }
 })
