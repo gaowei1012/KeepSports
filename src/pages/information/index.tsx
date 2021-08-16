3c68c2ae5a773eff14e7e4505ddf6f06fb71b2b5
gaowei1012 <gyb2020018@163.com>
2021-08-16 22:38:08 +0800

    添加聊天头部

--- src/pages/information/index.tsx
+++ src/pages/information/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import TopNavigationBar from '../../navigation/TopNavigationBar'
-
+import { styles } from '../../styles/view-style/info'
 
 const Information = () => {
   const statusbar = {
@@ -9,8 +9,22 @@ const Information = () => {
     barStyle: 'dark-content'
   }
   return (
-    <SafeAreaView>
-      <TopNavigationBar title={'聊天'} statusBar={statusbar} style={{ backgroundColor: '#ffffff' }} />
+    <SafeAreaView style={styles.container}>
+      <TopNavigationBar
+        title={'聊天'}
+        statusBar={statusbar}
+        style={{ backgroundColor: '#F7F7F7' }}
+        leftButton={
+          <TouchableOpacity activeOpacity={1}>
+            <Image style={styles.l_clear_icon} resizeMode='cover' source={require('../../assets/pages/info/clear.png')} />
+          </TouchableOpacity>
+        }
+        rightButton={
+          <TouchableOpacity activeOpacity={1}>
+            <Image style={styles.r_me_icon} resizeMode='cover' source={require('../../assets/pages/info/me.png')} />
+          </TouchableOpacity>
+        }
+      />
       <View>
         <Text>消息</Text>
       </View>
---
+++ src/styles/view-style/info.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+import { px2dp, width, height } from '../../utils/px2dp'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F7F7F7'
+  },
+  l_clear_icon: {
+    width: px2dp(21),
+    height: px2dp(24),
+  },
+  r_me_icon: {
+    width: px2dp(21),
+    height: px2dp(24),
+  }
+})
