90f7c11a00dfc98f2b5152de3d40109ac2d35642
gaowei1012 <gyb2020018@163.com>
2021-08-16 22:21:55 +0800

    添加聊天

--- src/pages/information/index.tsx
+++ src/pages/information/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
+import TopNavigationBar from '../../navigation/TopNavigationBar'
+
 
 const Information = () => {
+  const statusbar = {
+    backgroundColor: '#ffffff',
+    barStyle: 'dark-content'
+  }
   return (
     <SafeAreaView>
+      <TopNavigationBar title={'聊天'} statusBar={statusbar} style={{ backgroundColor: '#ffffff' }} />
       <View>
         <Text>消息</Text>
       </View>
