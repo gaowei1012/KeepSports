bba4b702f0b250308f564294bd9c744ba827f01e
gaowei1012 <gyb2020018@163.com>
2021-08-16 22:16:58 +0800

    添加发现页面

--- src/pages/discovery/index.tsx
+++ src/pages/discovery/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
+import TopNavigationBar from '../../navigation/TopNavigationBar'
 
 const Discovery = () => {
+  const statusbar = {
+    backgroundColor: '#ffffff',
+    barStyle: 'dark-content'
+  }
   return (
     <SafeAreaView>
+      <TopNavigationBar title={'发现'} statusBar={statusbar} style={{ backgroundColor: '#ffffff' }} />
       <View>
         <Text>发现</Text>
       </View>
