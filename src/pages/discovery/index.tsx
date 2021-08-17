6c80881e8d3fcf95d6fc9a3ab6e72a25230c605c
gaowei1012 <gyb2020018@163.com>
2021-08-17 23:12:24 +0800

    添加发现顶部

--- src/pages/discovery/index.tsx
+++ src/pages/discovery/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image, ImageBackground } from 'react-native'
 import TopNavigationBar from '../../navigation/TopNavigationBar'
+import { styles } from '../../styles/view-style/discovery'
+
+const DiscoveryData: any = [
+  { icon: require('../../assets/pages/discovery/l_icon.png'), title: '我的目标', r_icon: require('../../assets/pages/discovery/l.png'), desc: '已定制20项' },
+  { icon: require('../../assets/pages/discovery/r_icon.png'), title: '我的团队', r_icon: require('../../assets/pages/discovery/r.png'), desc: '已加入五项' }
+]
 
 const Discovery = () => {
   const statusbar = {
@@ -8,10 +14,22 @@ const Discovery = () => {
     barStyle: 'dark-content'
   }
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <TopNavigationBar title={'发现'} statusBar={statusbar} style={{ backgroundColor: '#ffffff' }} />
-      <View>
-        <Text>发现</Text>
+      <View style={styles.discovery_top_container}>
+        <View style={styles.discovery_top_flow}>
+          {DiscoveryData.map((item: { icon: any; r_icon: any; title: string; desc: string }) => (
+            <ImageBackground style={styles.discovery_top_bg} source={item.icon}>
+              <View style={styles.discovery_content_flow}>
+                <View>
+                  <Text style={styles.title}>{item.title}</Text>
+                  <Text style={styles.desc}>{item.desc}</Text>
+                </View>
+                <Image style={styles.icon} source={item.r_icon} />
+              </View>
+            </ImageBackground>
+          ))}
+        </View>
       </View>
     </SafeAreaView>
   )
--- src/styles/view-style/discovery.ts
+++ src/styles/view-style/discovery.ts
@@ -4,5 +4,42 @@ import { px2dp, width, height } from '../../utils/px2dp'
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  discovery_top_container: {
+    width: px2dp(345),
+    alignSelf: 'center'
+  },
+  discovery_top_flow: {
+    marginHorizontal: px2dp(6),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  discovery_top_bg: {
+    width: px2dp(147),
+    height: px2dp(75)
+  },
+  discovery_content_flow: {
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent: 'space-between',
+    paddingLeft: px2dp(17),
+    paddingTop: px2dp(17),
+    paddingRight: px2dp(13),
+    paddingBottom: px2dp(17)
+  },
+  icon: {
+    width: px2dp(41),
+    height: px2dp(38)
+  },
+  title: {
+    color:'#FFFFFF',
+    fontSize: px2dp(16),
+    fontWeight: 'bold'
+  },
+  desc: {
+    marginTop: px2dp(11),
+    color:'#FFFFFF',
+    fontSize: px2dp(12),
   }
 })
