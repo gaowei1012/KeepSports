3269ed82e24ca599667c70d64e057e37c9e02712
gaowei1012 <gyb2020018@163.com>
2021-08-17 23:38:22 +0800

    fix

--- src/pages/discovery/index.tsx
+++ src/pages/discovery/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, SafeAreaView, Image, ImageBackground } from 'react-native'
+import { View, Text, SafeAreaView, Image, ImageBackground, ScrollView } from 'react-native'
 import TopNavigationBar from '../../navigation/TopNavigationBar'
 import { styles } from '../../styles/view-style/discovery'
 
@@ -8,29 +8,59 @@ const DiscoveryData: any = [
   { icon: require('../../assets/pages/discovery/r_icon.png'), title: '我的团队', r_icon: require('../../assets/pages/discovery/r.png'), desc: '已加入五项' }
 ]
 
+const DiscoveryContentData: any = [
+  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '山西健身体操团队' },
+  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '山西健身体操团队' },
+  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '山西健身体操团队' },
+  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '山西健身体操团队' }
+]
+
 const Discovery = () => {
   const statusbar = {
-    backgroundColor: '#ffffff',
+    backgroundColor: '#FAFAFA',
     barStyle: 'dark-content'
   }
   return (
     <SafeAreaView style={styles.container}>
-      <TopNavigationBar title={'发现'} statusBar={statusbar} style={{ backgroundColor: '#ffffff' }} />
-      <View style={styles.discovery_top_container}>
-        <View style={styles.discovery_top_flow}>
-          {DiscoveryData.map((item: { icon: any; r_icon: any; title: string; desc: string }) => (
-            <ImageBackground style={styles.discovery_top_bg} source={item.icon}>
-              <View style={styles.discovery_content_flow}>
-                <View>
-                  <Text style={styles.title}>{item.title}</Text>
-                  <Text style={styles.desc}>{item.desc}</Text>
+      <TopNavigationBar title={'发现'} statusBar={statusbar} style={{ backgroundColor: '#FAFAFA' }} />
+      <ScrollView>
+        <View style={styles.discovery_top_container}>
+          <View style={styles.discovery_top_flow}>
+            {DiscoveryData.map((item: { icon: any; r_icon: any; title: string; desc: string }) => (
+              <ImageBackground style={styles.discovery_top_bg} source={item.icon}>
+                <View style={styles.discovery_content_flow}>
+                  <View>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Text style={styles.desc}>{item.desc}</Text>
+                  </View>
+                  <Image style={styles.icon} source={item.r_icon} />
                 </View>
-                <Image style={styles.icon} source={item.r_icon} />
-              </View>
+              </ImageBackground>
+            ))}
+          </View>
+        </View>
+        <View style={styles.discovery_content}>
+          <View style={styles.content_flow}>
+            <View style={styles.t_image_flow}>
+              <Text style={styles.td_text}>附近团队</Text>
+              <Image style={styles.wx} source={require('../../assets/base/wx.png')} />
+            </View>
+            <View style={styles.more_flow}>
+              <Text style={styles.more_text}>更多</Text>
+              <Image style={styles.more_image} source={require('../../assets/base/arrow.png')} />
+            </View>
+          </View>
+        </View>
+        <View style={styles.bottom_content}>
+          {DiscoveryContentData.map((item: { icon: any; icon_b: any; desc: string }) => (
+            <ImageBackground style={styles.bottom_content_flow} source={item.icon}>
+              <ImageBackground style={styles.bottom_content_c} source={item.icon_b}>
+                <Text style={styles.b_c_desc}>{item.desc}</Text>
+              </ImageBackground>
             </ImageBackground>
           ))}
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
--- src/styles/view-style/discovery.ts
+++ src/styles/view-style/discovery.ts
@@ -4,6 +4,7 @@ import { px2dp, width, height } from '../../utils/px2dp'
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FAFAFA'
   },
   discovery_top_container: {
     width: px2dp(345),
@@ -41,5 +42,69 @@ export const styles = StyleSheet.create({
     marginTop: px2dp(11),
     color:'#FFFFFF',
     fontSize: px2dp(12),
+  },
+  discovery_content: {
+    width: px2dp(345),
+    alignSelf: 'center',
+    marginTop: px2dp(31),
+  },
+  content_flow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: px2dp(6)
+  },
+  t_image_flow: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  wx: {
+    marginTop: px2dp(6),
+    width: px2dp(24),
+    height: px2dp(13)
+  },
+  td_text: {
+    fontSize: px2dp(18),
+    color: '#333333',
+    fontWeight: 'bold'
+  },
+  more_flow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  more_text: {
+    color: '#999999',
+    fontSize: px2dp(12),
+    marginRight: (5)
+  },
+  more_image: {
+    width: px2dp(6),
+    height: px2dp(10)
+  },
+  bottom_content: {
+    backgroundColor:'#fff',
+    alignItems: 'center'
+  },
+  bottom_content_flow: {
+    width: px2dp(353),
+    height: px2dp(192),
+    marginTop: px2dp(15),
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  bottom_content_c: {
+    width: px2dp(344),
+    height: px2dp(36),
+    marginBottom: px2dp(8),
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  b_c_desc: {
+    color: '#FFFFFF',
+    fontWeight:'bold',
+    fontSize: px2dp(16)
   }
 })
