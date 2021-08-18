2c2449d82e393e7e5375c2fb9624634640d2b597
gaowei1012 <gyb2020018@163.com>
2021-08-18 11:21:33 +0800

    fix

--- src/styles/view-style/dynamic.ts
+++ src/styles/view-style/dynamic.ts
@@ -1,8 +1,17 @@
+/*
+ * @Author: 执念
+ * @Date: 2021-08-18 09:09:02
+ * @LastEditTime: 2021-08-18 09:38:51
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /KeepSports/src/styles/view-style/dynamic.ts
+ */
 import { StyleSheet } from 'react-native'
 import { px2dp, width, height } from '../../utils/px2dp'
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#FAFAFA'
   }
 })
