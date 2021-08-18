6f0a71c29f9d00d0b0728f28d3c727a5d4e10ca3
gaowei1012 <gyb2020018@163.com>
2021-08-18 09:12:53 +0800

    添加动态

--- src/pages/dynamic/index.tsx
+++ src/pages/dynamic/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image, ImageBackground, ScrollView } from 'react-native'
+import TopNavigationBar from '../../navigation/TopNavigationBar'
+import { styles } from '../../styles/view-style/dynamic'
 
 const Dynamic = () => {
+  const statusbar = {
+    backgroundColor: '#FAFAFA',
+    barStyle: 'dark-content'
+  }
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <TopNavigationBar title={'动态'} statusBar={statusbar} style={{ backgroundColor: '#FAFAFA' }} />
       <View>
         <Text>动态</Text>
       </View>
---
+++ src/styles/view-style/dynamic.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native'
+import { px2dp, width, height } from '../../utils/px2dp'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
