05266e91117a20f584a0675dd08c063a13bf18e0
gaowei1012 <gyb2020018@163.com>
2021-08-19 17:04:28 +0800

    添加目标距离交互

--- src/pages/home/TargetDistance.tsx
+++ src/pages/home/TargetDistance.tsx
@@ -9,12 +9,49 @@ const statusbar = {
   barStyle: 'dark-content'
 }
 
+const jldata = [
+  { sum: 1.0, type: 1, c: '' },
+  { sum: 2.0, type: 2, c: '' },
+  { sum: 3.0, type: 3, c: '' },
+  { sum: 5.0, type: 4, c: '' },
+  { sum: 8.0, type: 5, c: '' },
+  { sum: 10.0, type: 6, c: '' },
+  { sum: 12.0, type: 7, c: '' },
+  { sum: 21.0975, type: 8, c: '半马' },
+  { sum: 42.195, type: 9, c: '全马' }
+]
+
 const TargetDistance = (props: any) => {
+  const [default_type, setDefaultType] = useState<number>(1)
+  const switchTab = (type: number) => {
+    setDefaultType(type)
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <TopNavigationBar title="设定距离目标" statusBar={statusbar} leftButton={GoBack(props)} style={{ backgroundColor: '#fff' }} />
-      <View>
-        <Text>目标距离</Text>
+      <TopNavigationBar title='设定距离目标' statusBar={statusbar} leftButton={GoBack(props)} style={{ backgroundColor: '#fff' }} />
+      <View style={styles.content}>
+        <View style={styles.target_top_jl}>
+          <Text style={styles.target_top_sum}>0.80</Text>
+          <Text style={styles.target_top_text}>距离步行公里</Text>
+        </View>
+        <View style={styles.target_content}>
+          {jldata.map((j) => (
+            <TouchableOpacity activeOpacity={0.8} onPress={() => switchTab(j.type)}>
+              <ImageBackground source={`${default_type !== j.type ? require('../../assets/pages/sports/nxz.png') : require('../../assets/pages/sports/xz.png')}`} style={styles.target_content_bg}>
+                {j.sum > 20 ? (
+                  <ImageBackground style={styles.tl} source={require('../../assets/pages/sports/tl.png')}>
+                    <Text style={styles.c}>{j.c}</Text>
+                  </ImageBackground>
+                ) : null}
+                <Text style={[default_type !== j.type ? styles.target_sum_text : styles.ac_target_sum_text]}>{j.sum}</Text>
+              </ImageBackground>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <TouchableOpacity style={styles.target_c} onPress={() => {}}>
+          <Text style={styles.target_c_text}>确定</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   )
--- src/styles/view-style/targetdistance.ts
+++ src/styles/view-style/targetdistance.ts
@@ -5,5 +5,76 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+  content: {
+    width: px2dp(345),
+    alignSelf: 'center'
+  },
+  target_top_jl: {
+    marginTop: px2dp(81),
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  target_top_sum: {
+    fontSize: px2dp(60),
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  target_top_text: {
+    fontSize: px2dp(14),
+    color: '#333',
+    marginTop: px2dp(16),
+    fontWeight: 'bold'
+  },
+  target_c: {
+    marginTop: px2dp(75),
+    backgroundColor: '#24C789',
+    width: px2dp(317),
+    height: px2dp(44),
+    alignSelf: 'center',
+    borderRadius: px2dp(22),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  target_c_text: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: px2dp(17)
+  },
+  target_sum_text: {
+    color: '#9A9FAD',
+    fontSize: px2dp(24),
+    fontWeight: 'bold'
+  },
+  ac_target_sum_text: {
+    color: '#333',
+    fontSize: px2dp(24),
+    fontWeight: 'bold'
+  },
+  target_content: {
+    marginTop: px2dp(36),
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around'
+  },
+  target_content_bg:{
+    width: px2dp(105),
+    height: px2dp(60),
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: px2dp(15)
+  },
+  tl: {
+    width: px2dp(20),
+    height: px2dp(13),
+    alignSelf: 'flex-start',
+    marginTop: px2dp(-16),
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  c: {
+    color: '#fff',
+    fontSize: px2dp(8),
+    fontWeight: '500'
   }
 })
