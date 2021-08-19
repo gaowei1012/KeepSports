197914fb915603b8afd96e0c1b6717746cc5e2ab
gaowei1012 <gyb2020018@163.com>
2021-08-19 15:29:09 +0800

    添加跑步状态

--- src/pages/home/Sports.tsx
+++ src/pages/home/Sports.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import { View, Text, SafeAreaView, Image, ImageBackground, TouchableOpacity } from 'react-native'
 import TopNavigationBar from '../../navigation/TopNavigationBar'
 import { styles } from '../../styles/view-style/sports'
@@ -22,21 +23,50 @@ const ydData = [
   }
 ]
 
+const ydDataStart = [
+  {
+    desc: '平均配速',
+    sum: '19‘20‘‘',
+    icon: require('../../assets/pages/sports/time.png')
+  },
+  {
+    desc: '用时',
+    sum: '16:20',
+    icon: require('../../assets/pages/sports/pb.png')
+  },
+  {
+    desc: '热量',
+    sum: '180',
+    icon: require('../../assets/pages/sports/rl.png')
+  }
+]
+
 const Sports = (props: any) => {
   const statusbar = {
     backgroundColor: '#DEF2EA',
     barStyle: 'dark-content'
+  }
+  const [start, setStart] = useState<boolean>(false)
+  // 开始运行
+  const sport_start = () => {
+    setStart(!start)
   }
   return (
     <SafeAreaView style={styles.container}>
       <TopNavigationBar statusBar={statusbar} leftButton={GoBack(props)} style={{ backgroundColor: '#DEF2EA' }} />
       <View style={styles.sports_container}>
         <View style={styles.sports_num_wrapper}>
-          <Text style={styles.sports_num}>00.00</Text>
+          <Text style={styles.sports_num}>{!start ? '00.00' : '10.26'}</Text>
           <Text style={styles.sports_gl}>公里</Text>
         </View>
         <ImageBackground style={styles.sports_bg_container} source={require('../../assets/pages/sports/ydbg.png')}>
-          {ydData.map((d) => (
+          {!start ? ydData.map((d) => (
+            <View style={styles.sports_c_w}>
+              <Image style={styles.sports_c_i} source={d.icon} />
+              <Text style={styles.sports_c_sum}>{d.sum}</Text>
+              <Text style={styles.desc}>{d.desc}</Text>
+            </View>
+          )) : ydDataStart.map((d) => (
             <View style={styles.sports_c_w}>
               <Image style={styles.sports_c_i} source={d.icon} />
               <Text style={styles.sports_c_sum}>{d.sum}</Text>
@@ -51,9 +81,9 @@ const Sports = (props: any) => {
               <Image style={styles.sports_mb_i} source={require('../../assets/pages/sports/r.png')} />
             </ImageBackground>
           </TouchableOpacity>
-          <TouchableOpacity>
-            <ImageBackground style={styles.sports_c_start} source={require('../../assets/pages/sports/start.png')}>
-              <Text style={styles.sports_c_start_text}>开始</Text>
+          <TouchableOpacity activeOpacity={0.8} onPress={sport_start}>
+            <ImageBackground style={styles.sports_c_start} source={`${!start ? require('../../assets/pages/sports/start.png') : require('../../assets/pages/sports/end.png')}`}>
+              <Text style={styles.sports_c_start_text}>{`${!start ? '开始' : '暂停'}`}</Text>
             </ImageBackground>
           </TouchableOpacity>
         </View>
