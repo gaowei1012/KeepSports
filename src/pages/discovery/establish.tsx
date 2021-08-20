3bad98fea1ba4a316635d14576c5ca1900899f33
gaowei1012 <gyb2020018@163.com>
2021-08-20 09:53:05 +0800

    添加创建团逻辑

--- src/pages/discovery/establish.tsx
+++ src/pages/discovery/establish.tsx
@@ -4,24 +4,49 @@ import TopNavigationBar from '../../navigation/TopNavigationBar'
 import { NavigationUtil } from '../../navigation/NavigationUtil'
 import { styles } from '../../styles/view-style/establish'
 import { GoBack } from '../../utils/goBack'
+import { useState } from 'react'
 
 const statusbar = {
-  backgroundColor: "rgba(0, 0, 0, 0)",
+  backgroundColor: 'rgba(0, 0, 0, 0)',
   barStyle: 'dark-content',
   translucent: true,
   hidden: false
 }
 
+const selData = [
+  { title: '选择头像', type: 1 },
+  { title: '设置密码', type: 2 },
+  { title: '确认创建', type: 3 }
+]
+
 const Establish = (props: any) => {
+  const [type, setType] = useState<number>(1)
   return (
-    <ImageBackground style={styles.establish_bg__container} source={require('../../assets/pages/establish/bg.png')}>
-      <SafeAreaView >
-        <TopNavigationBar leftButton={GoBack(props)} title={'创建团'} statusBar={statusbar} />
-        <ScrollView>
-          <Text>创建团</Text>
-        </ScrollView>
-      </SafeAreaView>
-    </ImageBackground>
+    <View>
+      <ImageBackground style={styles.establish_bg__container} source={require('../../assets/pages/establish/bg.png')}>
+        <SafeAreaView>
+          <TopNavigationBar leftButton={GoBack(props)} title={'创建团'} statusBar={statusbar} />
+          <ImageBackground style={styles.establish_bg__top} source={require('../../assets/pages/establish/tbg.png')}>
+            {selData.map((c) => (
+              <View key={c.type} style={styles.avatar_wrapper}>
+                <Image style={styles.avatar_icon} source={`${type == c.type ? require('../../assets/pages/establish/sr.png') : require('../../assets/pages/establish/nr.png')}`} />
+                <Text style={styles.avatar_text}>选择头像</Text>
+              </View>
+            ))}
+          </ImageBackground>
+        </SafeAreaView>
+      </ImageBackground>
+      <ImageBackground style={styles.establish_bg__content} source={require('../../assets/pages/establish/cbg.png')}>
+        <TouchableOpacity>
+          <ImageBackground source={require('../../assets/pages/establish/pc.png')} style={styles.establish_bg__content_photon}>
+            <Image style={styles.establish_bg__content_photon_icon} source={require('../../assets/pages/establish/c.png')} />
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.establish_bg__content_btn}>
+          <Text style={styles.establish_bg__content_next_text}>下一步</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </View>
   )
 }
 
--- src/styles/view-style/establish.ts
+++ src/styles/view-style/establish.ts
@@ -9,5 +9,59 @@ export const styles = StyleSheet.create({
   establish_bg__container: {
     width: width,
     height: px2dp(247)
+  },
+  establish_bg__top: {
+    width: px2dp(346),
+    height: px2dp(112),
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    marginTop: px2dp(29)
+  },
+  establish_bg__content: {
+    width: width,
+    height: px2dp(454),
+    alignItems: 'center',
+    marginTop: px2dp(101),
+  },
+  establish_bg__content_photon: {
+    width: px2dp(120),
+    height: px2dp(120),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  establish_bg__content_photon_icon: {
+    width: px2dp(39),
+    height: px2dp(33)
+  },
+  establish_bg__content_btn: {
+    width: px2dp(345),
+    height: px2dp(38),
+    borderRadius: px2dp(19),
+    backgroundColor: '#24C789',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: px2dp(180)
+  },
+  establish_bg__content_next_text: {
+    color: '#fff',
+    fontSize: px2dp(17),
+    fontWeight: 'bold'
+  },
+  avatar_wrapper: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  avatar_icon: {
+    width: px2dp(27),
+    height: px2dp(27)
+  },
+  avatar_text: {
+    color: '#333333',
+    fontSize: px2dp(13),
+    fontWeight: '500',
+    marginTop: px2dp(13)
   }
 })
