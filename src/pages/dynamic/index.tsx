6c83d8cf2c9148194c562ec79abd2be6eb0e025d
gaowei1012 <gyb2020018@163.com>
2021-08-21 10:41:16 +0800

    添加发现页面

--- src/pages/dynamic/index.tsx
+++ src/pages/dynamic/index.tsx
@@ -1,19 +1,139 @@
-import React from 'react'
-import { View, Text, SafeAreaView, Image, ImageBackground, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, SafeAreaView, Image, ImageBackground, ScrollView, TouchableOpacity } from 'react-native'
 import TopNavigationBar from '../../navigation/TopNavigationBar'
 import { styles } from '../../styles/view-style/dynamic'
 
-const Dynamic = () => {
-  const statusbar = {
-    backgroundColor: '#FAFAFA',
-    barStyle: 'dark-content'
+const dynamicData = [
+  { title: '全部', type: 1 },
+  { title: '好友', type: 2 },
+  { title: '同城', type: 3 }
+]
+
+const dylistData = [
+  {
+    title: '拯救地球李小姐',
+    date: '57分钟前',
+    content: '当一首歌突然好听，那一定多了一个故事#老照片#不忘初心，不负时光.',
+    comment_name: '爱蹦跶的小猴子',
+    comment: '哈哈哈，歌曲真的好治愈呦，烦恼的时 候在歌曲里真的可以找到答案',
+    icon_list: [
+      {
+        icon: require('../../assets/pages/dynamic/i1.png')
+      },
+      {
+        icon: require('../../assets/pages/dynamic/i2.png')
+      },
+      {
+        icon: require('../../assets/pages/dynamic/i3.png')
+      }
+    ]
+  },
+  {
+    title: '拯救地球李小姐',
+    date: '57分钟前',
+    content: '当一首歌突然好听，那一定多了一个故事#老照片#不忘初心，不负时光.',
+    comment_name: '爱蹦跶的小猴子',
+    comment: '哈哈哈，歌曲真的好治愈呦，烦恼的时 候在歌曲里真的可以找到答案',
+    icon_list: [
+      {
+        icon: require('../../assets/pages/dynamic/i1.png')
+      },
+      {
+        icon: require('../../assets/pages/dynamic/i2.png')
+      },
+      {
+        icon: require('../../assets/pages/dynamic/i3.png')
+      }
+    ]
+  },
+  {
+    title: '拯救地球李小姐',
+    date: '57分钟前',
+    content: '当一首歌突然好听，那一定多了一个故事#老照片#不忘初心，不负时光.',
+    comment_name: '爱蹦跶的小猴子',
+    comment: '哈哈哈，歌曲真的好治愈呦，烦恼的时 候在歌曲里真的可以找到答案',
+    icon_list: [
+      {
+        icon: require('../../assets/pages/dynamic/i1.png')
+      },
+      {
+        icon: require('../../assets/pages/dynamic/i2.png')
+      },
+      {
+        icon: require('../../assets/pages/dynamic/i3.png')
+      }
+    ]
   }
+]
+
+const Dynamic = () => {
+  const [type, setType] = useState<number>(1)
+
+  // 切换
+  const switchTab = (type: number) => {
+    setType(type)
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <TopNavigationBar title={'动态'} statusBar={statusbar} style={{ backgroundColor: '#FAFAFA' }} />
-      <View>
-        <Text>动态</Text>
+      <View style={styles.dynamic_top_switch}>
+        {dynamicData.map((d) => (
+          <TouchableOpacity style={styles.dynamic_top_title} onPress={() => switchTab(d.type)}>
+            <Text style={[type == d.type ? styles.action_title : styles.no_action_title]}>{d.title}</Text>
+            {type == d.type ? <Image style={styles.dynamic_top_icon} source={require('../../assets/pages/dynamic/sle.png')} /> : <View style={styles.no_dynamic_top_icon} />}
+          </TouchableOpacity>
+        ))}
       </View>
+      <ScrollView style={styles.dynamic_content_scrollview}>
+        <View style={styles.dynamic_content_flow__wrapper}>
+          {dylistData.map((list) => (
+            <View style={styles.dynamic_content_flow}>
+              <View style={styles.dy_top_avatar_wrapper}>
+                {/* 头像 */}
+                <View style={styles.dy_top_left_wrapper}>
+                  <Image source={require('../../assets/pages/dynamic/a1.png')} style={styles.avatar} />
+                  <View style={styles.avatar_right_wrapper}>
+                    <Text style={styles.avatar_text}>{list.title}</Text>
+                    <Text style={styles.avatar_desc}>{list.date}</Text>
+                  </View>
+                </View>
+                <TouchableOpacity>
+                  <ImageBackground source={require('../../assets/pages/dynamic/gz.png')} style={styles.dy_top_right_wrapper}>
+                    <Image source={require('../../assets/pages/dynamic/add.png')} style={styles.dy_top_right_add} />
+                    <Text style={styles.dy_top_right_add_text}>关注</Text>
+                  </ImageBackground>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.dy_content_wrapper}>
+                <Text>{list.content}</Text>
+                <View style={styles.content_img_wrapper}>
+                  {list.icon_list.map(img => (
+                    <Image source={img.icon} style={styles.content_img}/>
+                  ))}
+                </View>
+                <ImageBackground style={styles.dy_comment_new_icon} source={require('../../assets/pages/dynamic/pl.png')}>
+                  <Text style={styles.dy_comment_text}>最新评论</Text>
+                </ImageBackground>
+                <ImageBackground style={styles.dy_comment_wrapper} source={require("../../assets/pages/dynamic/commentbg.png")}>
+                  <Text style={styles.comment_name}>{list.comment_name}:</Text>
+                  <Text style={styles.comment_content}>{list.comment}</Text>
+                </ImageBackground>
+              </View>
+              <View style={styles.dy_bottom_wrapper}>
+                <View style={styles.dy_flow}>
+                  <Image style={styles.btn_icon} source={require('../../assets/pages/dynamic/dz.png')}/>
+                  <Text style={styles.btn_text}>12</Text>
+                </View>
+                <Image style={styles.dy_line} source={require('../../assets/pages/dynamic/line.png')}/>
+                <View style={styles.dy_flow}>
+                  <Image style={styles.btn_icon} source={require('../../assets/pages/dynamic/ly.png')}/>
+                  <Text style={styles.btn_text}>9</Text>
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
--- src/styles/view-style/dynamic.ts
+++ src/styles/view-style/dynamic.ts
@@ -13,5 +13,159 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FAFAFA'
+  },
+  dynamic_top_switch: {
+    width: px2dp(263),
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  dynamic_top_title: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  dynamic_top_icon: {
+    width: px2dp(24),
+    height: px2dp(13),
+    marginTop: px2dp(4)
+  },
+  no_dynamic_top_icon: {
+    width: px2dp(24),
+    height: px2dp(13),
+    marginTop: px2dp(4)
+  },
+  action_title: {
+    color: '#333333',
+    fontSize: px2dp(17),
+    fontWeight: 'bold'
+  },
+  no_action_title: {
+    color: '#666666',
+    fontSize: px2dp(17),
+    fontWeight: 'bold'
+  },
+  // content
+  dynamic_content_scrollview: {
+    marginTop: px2dp(10),
+    backgroundColor: '#F6F6F6'
+  },
+  dynamic_content_flow__wrapper: {
+    width: width,
+    backgroundColor: '#fff',
+    paddingVertical: px2dp(10)
+  },
+  dynamic_content_flow: {
+    width: px2dp(345),
+    alignSelf: 'center',
+    marginBottom: px2dp(20)
+  },
+  dy_top_avatar_wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  avatar_text: {
+    fontWeight: 'bold',
+    color: '#000000',
+    fontSize: px2dp(10)
+  },
+  avatar_desc: {
+    color: '#999999',
+    fontSize: px2dp(10),
+    marginTop: px2dp(8)
+  },
+  dy_top_left_wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  avatar: {
+    width: px2dp(36),
+    height: px2dp(36)
+  },
+  avatar_right_wrapper: {
+    marginLeft: px2dp(10)
+  },
+  dy_top_left_right_wrapper: {},
+  dy_top_right_wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: px2dp(59),
+    height: px2dp(25)
+  },
+  dy_top_right_add: {
+    width: px2dp(13),
+    height: px2dp(13)
+  },
+  dy_top_right_add_text: {
+    fontSize: px2dp(14),
+    color: '#24C789',
+    marginLeft: px2dp(2)
+  },
+  dy_content_wrapper: {
+    marginTop: px2dp(14),
+    width: px2dp(344),
+    alignSelf: 'center'
+  },
+  content_img_wrapper: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    marginVertical: px2dp(15)
+  },
+  content_img: {
+    width: px2dp(112),
+    height: px2dp(115)
+  },
+  dy_comment_wrapper: {
+    width: px2dp(346),
+    height: px2dp(67),
+    paddingHorizontal: px2dp(16),
+    paddingVertical: px2dp(10),
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  dy_comment_new_icon: {
+    width: px2dp(83),
+    height: px2dp(32),
+    alignItems: 'center'
+  },
+  dy_comment_text: {
+    marginTop: px2dp(7),
+    color: '#FC9422',
+    fontSize: px2dp(13)
+  },
+  comment_name: {
+    color: '#24C789',
+    fontSize: px2dp(13)
+  },
+  comment_content: {
+    color: '#666',
+    fontSize: px2dp(13)
+  },
+  dy_bottom_wrapper: {
+    paddingVertical: px2dp(17),
+    paddingHorizontal: px2dp(40),
+    width: px2dp(345),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  btn_icon: {
+    width: px2dp(19),
+    height: px2dp(18)
+  },
+  dy_line: {
+    height: px2dp(10)
+  },
+  dy_flow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  btn_text: {
+    color: '#333333',
+    fontSize: px2dp(13),
+    marginLeft: px2dp(15)
   }
 })
