9cc9e1250685237163da2ccffd52491228a5d3b9
gaowei1012 <gyb2020018@163.com>
2021-08-22 21:52:30 +0800

    add Information

---
+++ src/pages/discovery/Information.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, SafeAreaView, Image, ImageBackground, ScrollView, TouchableOpacity, StatusBar } from 'react-native'
+import * as ImagePicker from 'react-native-image-picker'
+import TopNavigationBar from '../../navigation/TopNavigationBar'
+import { NavigationUtil } from '../../navigation/NavigationUtil'
+import { styles } from '../../styles/view-style/establish'
+import { GoBack } from '../../utils/goBack'
+import { useState } from 'react'
+
+
+const Information = () => {
+  return (
+    <View>
+      <Text>Information</Text>
+    </View>
+  );
+}
+
+export default Information;
