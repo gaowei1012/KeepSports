6db335ea0ef6fb412b05002272147dccb1b40487
gaowei1012 <gyb2020018@163.com>
2021-08-28 13:42:30 +0800

    fix

--- src/pages/discovery/index.tsx
+++ src/pages/discovery/index.tsx
@@ -9,7 +9,7 @@
 import React from 'react'
 import { View, Text, SafeAreaView, Image, ImageBackground, ScrollView, TouchableOpacity } from 'react-native'
 import TopNavigationBar from '../../navigation/TopNavigationBar'
-import { NavigationUtil  } from '../../navigation/NavigationUtil'
+import { NavigationUtil } from '../../navigation/NavigationUtil'
 import { styles } from '../../styles/view-style/discovery'
 const statusbar = {
   backgroundColor: '#FAFAFA',
@@ -25,9 +25,9 @@ const DiscoveryData: any = [
 
 const DiscoveryContentData: any = [
   { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '山西健身体操团队' },
-  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '山西健身体操团队' },
-  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '山西健身体操团队' },
-  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '山西健身体操团队' }
+  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '武汉健身体操团队' },
+  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '北京健身体操团队' },
+  { icon: require('../../assets/pages/discovery/js.png'), icon_b: require('../../assets/pages/discovery/mb.png'), desc: '山东健身体操团队' }
 ]
 
 const Discovery = () => {
@@ -40,6 +40,7 @@ const Discovery = () => {
       NavigationUtil.goPage({}, 'Intent')
     }
   }
+
   return (
     <SafeAreaView style={styles.container}>
       <TopNavigationBar title={'发现'} statusBar={statusbar} style={{ backgroundColor: '#FAFAFA' }} />
@@ -47,7 +48,7 @@ const Discovery = () => {
         <View style={styles.discovery_top_container}>
           <View style={styles.discovery_top_flow}>
             {DiscoveryData.map((item: { icon: any; r_icon: any; title: string; desc: string }) => (
-              <TouchableOpacity onPress={() => establish(item.title)} style={styles.discovery_top_bg}>
+              <TouchableOpacity activeOpacity={1} onPress={() => establish(item.title)} style={styles.discovery_top_bg}>
                 <ImageBackground style={styles.discovery_top_bg} source={item.icon}>
                   <View style={styles.discovery_content_flow}>
                     <View>
@@ -75,7 +76,7 @@ const Discovery = () => {
         </View>
         <View style={styles.bottom_content}>
           {DiscoveryContentData.map((item: { icon: any; icon_b: any; desc: string }) => (
-            <TouchableOpacity onPress={() => {
+            <TouchableOpacity activeOpacity={1} onPress={() => {
               NavigationUtil.goPage({}, 'GroupInformation')
             }}>
               <ImageBackground style={styles.bottom_content_flow} source={item.icon}>
