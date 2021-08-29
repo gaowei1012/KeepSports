5c6df3671076e52a590c30f658a14599b61d6b6f
gaowei1012 <gyb2020018@163.com>
2021-08-29 16:52:16 +0800

    api

--- src/expand/api.ts
+++ src/expand/api.ts
@@ -1,5 +1,6 @@
 const API = {
     base_url: 'http://127.0.0.1:3000', // 本地代理
+    // base_url: 'http://api.zsmj88.xyz',
     login: '/user/login',
     register: '/user/register',
     getUserInfo: '/user/getUserInfo',
