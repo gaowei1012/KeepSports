41bab838af31847394e808b967f445011af7356d
gaowei1012 <gyb2020018@163.com>
2021-11-23 09:17:05 +0800

    fix components/Buttom

--- src/components/bottom/index.tsx
+++ src/components/bottom/index.tsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import { styles } from '../../styles/components/bottom'
-import { TouchableOpacity, Text } from 'react-native'
+import { TouchableOpacity, Text, ButtonProps } from 'react-native'
 
-interface IBottomType {
+interface IBottomType extends ButtonProps {
   text: string
-  activeOpacity?: number
-  onPress: () => void
   propStyles?: any
 }
 
 const Bottom = (props: IBottomType) => {
-  const { text, onPress, propStyles, activeOpacity } = props
+  const { text, propStyles } = props
 
   return (
     <TouchableOpacity
-      style={[styles.bottomContainer, propStyles]}
-      activeOpacity={activeOpacity}
-      onPress={onPress}>
+      style={[styles.bottomContainer, propStyles]}>
       <Text style={styles.text}>{text}</Text>
     </TouchableOpacity>
   )
