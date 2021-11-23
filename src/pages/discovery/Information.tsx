3442ce4cbb357f20d9a4bbc7d7f09294f37cb583
gaowei1012 <gyb2020018@163.com>
2021-11-23 09:42:29 +0800

    fix

--- src/pages/discovery/Information.tsx
+++ src/pages/discovery/Information.tsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text, SafeAreaView, Image, ImageBackground, ScrollView, TouchableOpacity, DeviceEventEmitter } from 'react-native'
+import { View, Text, SafeAreaView, Image, ImageBackground, ScrollView, TouchableOpacity } from 'react-native'
 import TopNavigationBar from '../../navigation/TopNavigationBar'
-import { NavigationUtil } from '../../navigation/NavigationUtil'
 import { styles } from '../../styles/view-style/establish'
 import { GoBack } from '../../utils/goBack'
 import { height } from '../../utils/px2dp'
@@ -13,17 +11,17 @@ const statusbar = {
 }
 
 const GroupInformation = (props: any) => {
-  const {data} =  props.navigation.state.params
-  
+  const { data } = props.navigation.state.params
+
   return (
     <View style={styles.group_container}>
       <ImageBackground style={styles.establish_bg__container} source={require('../../assets/pages/establish/bg.png')}>
-        <SafeAreaView style={{height: height}}>
+        <SafeAreaView style={{ height: height }}>
           <TopNavigationBar leftButton={GoBack(props, true)} title={'团资料'} statusBar={statusbar} />
           <ScrollView>
             <ImageBackground style={styles.information_top_container} source={require('../../assets/pages/discovery/group.png')}>
               <View style={styles.information_avatar_wrapper}>
-                <Image style={styles.information_avatar_img} source={{uri: `data:image/png;base64,${data.icon}`}} />
+                <Image style={styles.information_avatar_img} source={{ uri: `data:image/png;base64,${data.icon}` }} />
                 <View style={styles.information_avatar_name}>
                   <Text style={styles.information_t_t}>{data.title}</Text>
                   <Text style={styles.information_t_h}>团号：{data.bh}</Text>
@@ -47,7 +45,7 @@ const GroupInformation = (props: any) => {
               </View>
               <View style={styles.group_t_tips}>
                 <Text style={styles.group_t_tips_title}>团主</Text>
-                <Image style={styles.group_t_tips_img} source={{uri: `data:image/png;base64,${data.icon}`}} />
+                <Image style={styles.group_t_tips_img} source={{ uri: `data:image/png;base64,${data.icon}` }} />
               </View>
             </ImageBackground>
             <TouchableOpacity activeOpacity={1} style={styles.exit_wrapper}>
